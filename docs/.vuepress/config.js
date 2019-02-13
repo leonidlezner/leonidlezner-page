c9d15c69b0f075aca7b48e7710b0fadbd66c4787
Leonid Lezner <leonidlezner@users.noreply.github.com>
2019-02-13 15:51:25 +0100

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,29 @@
 module.exports = {
     base: "leonidlezner.github.io",
     title: "Leonid's blog",
-    description: "The small knowledge bucket of Leonid Lezner"
+    description: "The small knowledge bucket of Leonid Lezner",
+    
+    themeConfig:{
+            nav: [
+                { text: 'Quellen', link: '/quellen/' },
+                { text: 'Toolbox', link: '/toolbox/' },
+            ],
+            sidebar: [
+                {
+                    title: 'Counter',
+                    collapsable: false,
+                    children: [
+                    '/counter/counter-app'
+                    ]
+                },
+                {
+                    title: 'API Guide',
+                    collapsable: false,
+                    children: [
+                        '/guide/guide',
+                        '/guide/api'
+                    ]
+                }
+                ]
+        }
 };
