898f7a5cedfa5176e8466c3eb7393c8297d0031b
Leonid Lezner <leonidlezner@users.noreply.github.com>
2019-02-13 16:01:51 +0100

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base: "leonidlezner.github.io",
+    base: "https://leonidlezner.github.io",
     title: "Leonid's blog",
     description: "The small knowledge bucket of Leonid Lezner",
     
